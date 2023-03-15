ab205b2fc7475d1e00ab796130460fbdf371665e
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-03-15 16:38:41 -0400

    Add files via upload

---
+++ pokemon_app/server.js
@@ -0,0 +1,59 @@
+
+// setup dot env 
+require('dotenv').config()
+
+
+// Load express
+const express = require('express')
+
+// Setup our Express app
+const app = express()
+
+const PORT = 3030
+
+
+// Load the connectDB function
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB()
+
+
+// Loading data
+const pokemon = require('./models/pokemon')
+
+const pokemonRoutes = require('./routes/pokemonRoutes')
+
+
+// Load the create engine -> (npm install jsx-view-engine react react-dom)
+const { createEngine } = require('jsx-view-engine')
+
+// Configure the view engine and look for files ending in jsx
+app.set('view engine', 'jsx')
+
+// Create the engine and accept files ending in jsx
+app.engine('jsx', createEngine())
+
+// middleware 
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!')
+})
+
+
+
+app.use('/pokemon', pokemonRoutes)
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('Listening to the port: ' + PORT)
+})
+
+
+
+
